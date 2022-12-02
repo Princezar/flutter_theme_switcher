3e122a81f6144c7e0ecc3ebac5466e1b5e66c90a
SolenergyRD <elliezar_jariolne@solenergy.com.ph>
2022-12-02 09:43:44 +0800

    update Drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,20 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       drawer: const DrawerWidget(),
       body: Center(
-        child: SwitchListTile(
-          title: Text(themeProvider.getDarkTheme ? 'Dark': 'Light'),
-            secondary: Icon(
-              themeProvider.getDarkTheme ? Icons.dark_mode : Icons.light_mode,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-          value: themeProvider.getDarkTheme,
-          onChanged: (bool value){
-            setState(() {
-              themeProvider.setDarkTheme = value;
-            });
-          },
-
-        ),
+        child: Container(),
 
       ),
     );
--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:news_app_flutter_course/widgets/vertical_spacing_widget.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/theme_provider.dart';
 
 class DrawerWidget extends StatefulWidget {
   const DrawerWidget({Key? key}) : super(key: key);
@@ -12,6 +15,7 @@ class DrawerWidget extends StatefulWidget {
 class _DrawerWidgetState extends State<DrawerWidget> {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context); //them_provider declaration
     return Drawer(
       child: Material(
         child: ListView(
@@ -32,10 +36,32 @@ class _DrawerWidgetState extends State<DrawerWidget> {
                   ],
                 ),
             ),
-            ListTile(
-              leading: Icon(IconlyBold.home),
-              title: Text("Home",
-              style: TextStyle(fontSize: 20),),
+            listTiles(
+              icon: Icons.home,
+                label: "Home",
+                fct: (){},
+            ),
+            listTiles(
+              icon: Icons.bookmark,
+              label: "Bookmark",
+              fct: (){},
+            ),
+            const Divider(thickness: 2,),
+            SwitchListTile(
+              title: Text(themeProvider.getDarkTheme ? 'Dark': 'Light',
+              style: const TextStyle(fontSize: 20),
+              ),
+              secondary: Icon(
+                themeProvider.getDarkTheme ? Icons.dark_mode : Icons.light_mode,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              value: themeProvider.getDarkTheme,
+              onChanged: (bool value){
+                setState(() {
+                  themeProvider.setDarkTheme = value;
+                });
+              },
+
             )
           ],
         ),
@@ -44,3 +70,27 @@ class _DrawerWidgetState extends State<DrawerWidget> {
     );
   }
 }
+
+
+class listTiles extends StatelessWidget {
+  const listTiles({Key? key, required this.label, required this.fct, required this.icon}) : super(key: key);
+
+  final String label;
+  final Function fct;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading:Icon(icon,
+      color: Theme.of(context).colorScheme.secondary,
+      ),
+      title: Text(label,
+        style: const TextStyle(fontSize: 20),
+      ),
+      onTap: (){
+        fct();
+      },
+    );
+  }
+}
+
